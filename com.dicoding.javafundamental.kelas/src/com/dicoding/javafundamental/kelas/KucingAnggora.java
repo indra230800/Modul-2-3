f3cf8dcf38ab0c991fd799b6272d722f74521ffb
indra230800 <74169208+indra230800@users.noreply.github.com>
2021-01-11 12:21:51 +0700

    Class

---
+++ com.dicoding.javafundamental.kelas/src/com/dicoding/javafundamental/kelas/KucingAnggora.java
@@ -0,0 +1,12 @@
+package com.dicoding.javafundamental.kelas;
+
+public class KucingAnggora {
+   
+   String namaKucing;
+    int beratBadan; 
+    
+    public KucingAnggora(String nama, int berat){
+       namaKucing = nama;
+       beratBadan = berat;
+    }
+}
