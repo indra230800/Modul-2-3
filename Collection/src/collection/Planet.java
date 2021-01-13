b9ea15c4e6e18447688c4fb0e0b37ec70056a074
indra230800 <74169208+indra230800@users.noreply.github.com>
2021-01-13 10:39:34 +0700

    Collection

---
+++ Collection/src/collection/Planet.java
@@ -0,0 +1,16 @@
+package collection;
+
+class Planet {
+    private String name;
+    private double mass;
+    
+    public Planet(String name, double mass) {
+        this.name = name;
+        this.mass = mass;
+    }
+    
+    @Override
+    public String toString() {
+        return "Planet " + name + ", mass: " + mass;
+    }
+}
