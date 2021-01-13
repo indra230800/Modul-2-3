4eff85b8589cf0c5f30afc7e4e5515b67cd382e0
indra230800 <74169208+indra230800@users.noreply.github.com>
2021-01-13 09:50:53 +0700

    Object

---
+++ com.dicoding.javafundamental.obyek/src/com/dicoding/javafundamental/obyek/Hewan.java
@@ -0,0 +1,8 @@
+package com.dicoding.javafundamental.obyek;
+
+public class Hewan {
+    public static void cetakNama(String nama) {
+        System.out.println("Nama hewan: " + nama);
+    }
+    
+}
