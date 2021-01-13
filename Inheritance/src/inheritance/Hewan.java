d565c38e58cf7a50c8eee8e2028081a11b632fd9
indra230800 <74169208+indra230800@users.noreply.github.com>
2021-01-13 10:29:04 +0700

    Inheritance

---
+++ Inheritance/src/inheritance/Hewan.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class Hewan {
+    public Hewan() {
+        System.out.println("contruct Hewan");
+    }
+    
+    public void makan() { // base method
+        System.out.println("Hewan sedang makan..");
+    }
+}
